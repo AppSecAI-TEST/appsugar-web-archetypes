eeb0c4f409fdf59a737a43034159605c23843acf
shenliuyang <shenliuyang@gmail.com>
2016-04-29 15:08:37 +0800

    add source code

---
+++ trunk/environment.groovy
@@ -0,0 +1,32 @@
+ environments{
+	//default properties
+	base{
+		encoding = "utf-8"
+		db{
+			name = "appsugar-archetypes"
+		}
+		test{
+			jdbc{
+				groupId = "com.h2database"
+				artifactId = "h2"
+				version = "1.4.191"
+				url = "jdbc:h2:~/tmp/${db.name}"
+				driverClassName = "org.h2.Driver"
+				username = "sa"
+				password = "";
+			}
+			hibernate{
+				dialect = "org.hibernate.dialect.H2Dialect"
+			}
+		}
+		
+		jdbc = test.jdbc
+		
+		dbunit{
+			dataTypeFactoryName = "org.dbunit.ext.h2.H2DataTypeFactory";
+			operationType = "CLEAN_INSERT";
+			sampleData =  "/src/test/resources/data/sample-data.xml";
+		}
+	}
+	//other properties
+}
