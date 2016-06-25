41465300aa730d1cbffcba6a6a98ca7c5fa94ec5
shenliuyang <shenliuyang@gmail.com>
2016-06-25 18:01:57 +0800

    both original deploy and spring boot are support

--- appsugar-archetypes-web/src/main/java/org/appsugar/Application.java
+++ appsugar-archetypes-web/src/main/java/org/appsugar/Application.java
@@ -2,15 +2,27 @@ package org.appsugar;
 
 import org.appsugar.controller.ControllerConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * 程序启动入口
  * @author NewYoung
  * 2016年6月25日下午1:59:09
  */
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(getResources());
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(new Object[] { ControllerConfiguration.class, SecurityConfiguration.class,
-				ResourceConfiguration.class, ServiceConfiguration.class, RepositoryConfiguration.class }, args);
+		SpringApplication.run(getResources(), args);
+	}
+
+	public static Object[] getResources() {
+		return new Object[] { ControllerConfiguration.class, SecurityConfiguration.class, ResourceConfiguration.class,
+				ServiceConfiguration.class, RepositoryConfiguration.class };
 	}
 }
