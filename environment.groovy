a5f1fa4afa753e05e0fe4d8973c0daae854d6276
shenliuyang <shenliuyang@gmail.com>
2016-07-07 13:25:06 +0800

    add prod environment

--- environment.groovy
+++ environment.groovy
@@ -1,9 +1,9 @@
  environments{
-	//default properties
+	//默认测试环境
 	base{
 		encoding = "utf-8"
 		db{
-			name = "appsugar-archetypes"
+			name = "appsugar"
 		}
 		test{
 			jdbc{
@@ -18,16 +18,18 @@
 			hibernate{
 				dialect = "org.hibernate.dialect.H2Dialect"
 			}
+			dbunit{
+				dataTypeFactoryName = "org.dbunit.ext.h2.H2DataTypeFactory";
+				operationType = "CLEAN_INSERT";
+				sampleData =  "/src/test/resources/data/sample-data.xml";
+			}
 		}
 		
 		jdbc = test.jdbc
 		hibernate = test.hibernate
-		dbunit{
-			dataTypeFactoryName = "org.dbunit.ext.h2.H2DataTypeFactory";
-			operationType = "CLEAN_INSERT";
-			sampleData =  "/src/test/resources/data/sample-data.xml";
-		}
 	}
+	
+	//本机测试环境
 	mysql{
 		db{
 			name = "appsugar"
@@ -47,10 +49,28 @@
 		test{
 			jdbc = jdbc
 			hibernate = hibernate
-		}
-		dbunit{
-			dataTypeFactoryName = "org.dbunit.ext.mysql.MySqlDataTypeFactory";
+			dbunit{
+				dataTypeFactoryName = "org.dbunit.ext.mysql.MySqlDataTypeFactory";
+			}
 		}
 	}
 	
+	//正式环境
+	prod{
+		db{
+			name = "appsugar"
+		}
+		jdbc {
+			groupId = "mysql"
+			artifactId = "mysql-connector-java"
+			version = "5.1.39"
+			url = "jdbc:mysql://localhost:3306/${db.name}?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=utf-8"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "root"
+			password = "123456"
+		}
+		hibernate{
+			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+		}
+	}
 }
