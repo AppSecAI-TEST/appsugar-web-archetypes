b2eb918fcdc92930eb5a8becf26a66f6280d0f9a
shenliuyang <shenliuyang@gmail.com>
2016-11-09 13:19:57 +0800

    add source code

---
+++ appsugar-archetypes-api/src/main/java/org/appsugar/Application.java
@@ -0,0 +1,28 @@
+package org.appsugar;
+
+import org.appsugar.controller.ControllerConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 程序启动入口
+ * @author NewYoung
+ * 2016年6月25日下午1:59:09
+ */
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(getResources());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(getResources(), args);
+	}
+
+	public static Object[] getResources() {
+		return new Object[] { ControllerConfiguration.class, SecurityConfiguration.class, ResourceConfiguration.class,
+				ServiceConfiguration.class, RepositoryConfiguration.class };
+	}
+}
---
+++ appsugar-archetypes-api/src/main/java/org/appsugar/ResourceConfiguration.java
@@ -0,0 +1,25 @@
+package org.appsugar;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * 资源配置
+ * @author NewYoung
+ * 2016年6月25日上午11:03:22
+ */
+@Configuration
+public class ResourceConfiguration {
+
+	/**
+	 * 国际化消息
+	 */
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setFallbackToSystemLocale(false);
+		source.setBasenames("messages/ApplicationResources");
+		return source;
+	}
+}
