12abb82a31e18eb0b996b5628217d23b6b5dc132
dev <dev@dev-001-PC>
2017-08-10 16:47:05 +0800

    remove useless controller

--- appsugar-archetypes-web/src/main/java/org/appsugar/controller/MainController.java
+++ appsugar-archetypes-web/src/main/java/org/appsugar/controller/MainController.java
@@ -3,14 +3,23 @@ package org.appsugar.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.appsugar.domain.Menu;
 import org.appsugar.domain.MenuGroup;
+import org.appsugar.domain.Response;
 import org.appsugar.security.ShiroUtils;
 import org.appsugar.util.MenuUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Throwables;
 
 /**
  *
@@ -44,4 +53,22 @@ public class MainController {
 		return result;
 	}
 
+	/**
+	 * 用户登录
+	 * @param username 账号
+	 * @param password 密码
+	 * @param rememberMe 是否记住自己
+	 */
+	@RequestMapping("/login")
+	public Response<Void> login(@RequestParam String username, @RequestParam String password, Boolean rememberMe) {
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password,
+				MoreObjects.firstNonNull(rememberMe, Boolean.FALSE));
+		try {
+			subject.login(token);
+		} catch (AuthenticationException ex) {
+			return Response.error("username or password error " + Throwables.getRootCause(ex).getMessage());
+		}
+		return Response.SUCCESS;
+	}
 }
